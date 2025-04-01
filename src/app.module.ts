93808d5df90e3cc877e7cf8b3ba766ae4691847e
sajeebro <sajithmadusanka720@gmail.com>
2025-04-01 21:53:03 +0530

    registed all Mservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,13 @@ import { USER_PACKAGE_NAME, USER_SERVICE_NAME } from './user/types';
 
 @Module({
   imports: [
-    // Load configuration from .env
+    
     ConfigModule.forRoot({ isGlobal: true }),
 
     // Register microservices (gRPC clients)
     ClientsModule.register([
+
+         // Register the user service client------------------
       {
         name: USER_SERVICE_NAME,
         transport: Transport.GRPC,
@@ -25,7 +27,60 @@ import { USER_PACKAGE_NAME, USER_SERVICE_NAME } from './user/types';
           package: USER_PACKAGE_NAME,
           protoPath: join(__dirname, '../../proto/user.proto'),
         },
-      }
+      },
+     
+      // Register the delivery service client------------------------
+      {
+        name: 'DELIVERY_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.DELIVERY_SERVICE_URL || 'localhost:50053',
+          package: 'delivery',
+          protoPath: join(__dirname, '../../proto/delivery.proto'),
+        },
+      },
+      // Register the notification service client-------------------------
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.NOTIFICATION_SERVICE_URL || 'localhost:50054',
+          package: 'notification',
+          protoPath: join(__dirname, '../../proto/notification.proto'),
+        },
+      },
+      // Register the order service client-------------------------
+      {
+        name: 'ORDER_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.ORDER_SERVICE_URL || 'localhost:50055',
+          package: 'order',
+          protoPath: join(__dirname, '../../proto/order.proto'),
+        },
+      },
+       // Register the payment service client--------------------------
+       {
+        name: 'PAYMENT_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.PAYMENT_SERVICE_URL || 'localhost:50056',
+          package: 'payment',
+          protoPath: join(__dirname, '../../proto/payment.proto'),
+        },
+      },
+      // Register the restaurant service client---------------------
+      {
+        name: 'RESTAURANT_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.RESTAURANT_SERVICE_URL || 'localhost:50057',
+          package: 'restaurant',
+          protoPath: join(__dirname, '../../proto/restaurant.proto'),
+        },
+      },
+      
+
     ]),
   ],
   controllers: [
