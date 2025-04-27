c1528c8c05329b1a4962c3801b0e2cf9a3d3b951
Ravindu Ishan <IT22358752@my.sliit.lk>
2025-04-27 06:54:55 +0530

    Email Module Registered on API Gateway at default port 5058

---
+++ src/email/email.service.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+import { ClientGrpc } from '@nestjs/microservices';
+import { EMAIL_SERVICE_NAME, EmailAddress, EmailServiceClient, RejectionEmail, Response } from './types/email';
+
+@Injectable()
+export class EmailService implements OnModuleInit {
+  private emailServiceClient: EmailServiceClient;
+  constructor(@Inject(EMAIL_SERVICE_NAME) private readonly client: ClientGrpc) {}
+  onModuleInit() {
+    this.emailServiceClient = this.client.getService<EmailServiceClient>(EMAIL_SERVICE_NAME);
+  }
+
+  sendRegConfirmEmail(request: EmailAddress): Observable<Response> {
+    return this.emailServiceClient.sendRestaurantRegConfirmationEmail(request);
+  }
+  sendRegRejectEmail(request: RejectionEmail): Observable<Response> {
+    return this.emailServiceClient.sendRejectionEmail(request);
+  }
+}
